93f7d20622ce43935253beb1a776f0fca904d438
chloecookie <chloechia@gmail.com>
2023-03-14 21:17:03 -0700

    s

--- models/alexnet.py
+++ models/alexnet.py
@@ -1,55 +1,43 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torch import nn
+
+
 class AlexNet(nn.Module):
-    "Alexnet"
-    def __init__(self, num_classes=10):
-        super(AlexNet, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
-            nn.BatchNorm2d(96),
+    def __init__(self, num_classes: int = 200) -> None:
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2), #filters kwarg?
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 3, stride = 2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
-            nn.BatchNorm2d(256),
+            nn.MaxPool2d(3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 3, stride = 2))
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(384),
-            nn.ReLU())
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(384),
-            nn.ReLU())
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
+            nn.MaxPool2d(3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 3, stride = 2))
-        self.fc = nn.Sequential(
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(3, stride=2),
+            nn.AdaptiveAvgPool2d((6, 6))
+        )
+        self.classifier = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(9216, 4096),
-            nn.ReLU())
-        self.fc1 = nn.Sequential(
+            nn.Linear(256*6*6, 4096),
+            nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(4096, 4096),
-            nn.ReLU())
-        self.fc2= nn.Sequential(
-            nn.Linear(4096, num_classes))
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        out = self.fc1(out)
-        out = self.fc2(out)
-        return out`
-
-
+            nn.ReLU(),
+            nn.Linear(4096, num_classes),
+        )
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+    
 # class AlexNet(nn.Module):
 #     """Fake LeNet with 32x32 color images and 200 classes"""
 #     def __init__(self, num_classes: int = 200) -> None:
