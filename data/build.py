fb6fa32df9c2e668f2d6fe430828a7132950ca8f
Chloe Chia <76987180+chloecookie@users.noreply.github.com>
2023-03-15 22:44:29 -0700

    Update build.py

--- data/build.py
+++ data/build.py
@@ -1,42 +1,31 @@
-from torch.utils.data import DataLoader
+from .alexnet import AlexNet
+from .lenet import LeNet
+from .resnet18 import ResNet18
+from .resnet34 import ResNet34
 
-from data.datasets import CIFAR10Dataset, MediumImagenetHDF5Dataset
+def build_model(config):
+    "Model builder."
 
+    model_type = config.MODEL.NAME
 
-def build_loader(config):
-    if config.DATA.DATASET == "cifar10":
-        dataset_train = CIFAR10Dataset(img_size=config.DATA.IMG_SIZE, train=True)
-        dataset_val = CIFAR10Dataset(img_size=config.DATA.IMG_SIZE, train=False)
-        dataset_test = CIFAR10Dataset(img_size=config.DATA.IMG_SIZE, train=False)
-    elif config.DATA.DATASET == "medium_imagenet":
-        dataset_train = MediumImagenetHDF5Dataset(config.DATA.IMG_SIZE, split="train", augment=True)
-        dataset_val = MediumImagenetHDF5Dataset(config.DATA.IMG_SIZE, split="val", augment=False)
-        dataset_test = MediumImagenetHDF5Dataset(config.DATA.IMG_SIZE, split="test", augment=False)
+    if model_type == 'lenet':
+        model = LeNet(num_classes=config.MODEL.NUM_CLASSES)
+    elif model_type == 'resnet18':
+        model = ResNet18(num_classes=config.MODEL.NUM_CLASSES)
+    elif model_type == 'resnet34':
+        model = ResNet34(num_classes=config.MODEL.NUM_CLASSES)
+    elif model_type == 'alexnet':
+        model = AlexNet(num_classes=config.MODEL.NUM_CLASSES)
+    # elif model_type == 'resnet34':
+    #     model = ResNet34(num_classes=config.MODEL.NUM_CLASSES)
+    # elif model_type == 'resnet50':
+    #     model = ResNet50(num_classes=config.MODEL.NUM_CLASSES)
+    # elif model_type == 'resnet101':
+    #     model = ResNet101(num_classes=config.MODEL.NUM_CLASSES)
+    # elif model_type == 'resnet152':
+    #     model = ResNet152(num_classes=config.MODEL.NUM_CLASSES)
     else:
-        raise NotImplementedError
-
-    data_loader_train = DataLoader(
-        dataset_train,
-        batch_size=config.DATA.BATCH_SIZE,
-        shuffle=True,
-        num_workers=config.DATA.NUM_WORKERS,
-        pin_memory=config.DATA.PIN_MEMORY,
-    )
-
-    data_loader_val = DataLoader(
-        dataset_val,
-        batch_size=config.DATA.BATCH_SIZE,
-        shuffle=False,
-        num_workers=config.DATA.NUM_WORKERS,
-        pin_memory=False,
-    )
-
-    data_loader_test = DataLoader(
-        dataset_test,
-        batch_size=config.DATA.BATCH_SIZE,
-        shuffle=False,
-        num_workers=config.DATA.NUM_WORKERS,
-        pin_memory=False,
-    )
-
-    return dataset_train, dataset_val, dataset_test, data_loader_train, data_loader_val, data_loader_test
+        raise NotImplementedError(f"Unknown model: {model_type}")
+    
+    return model
+ 
